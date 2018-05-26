a44761bb8d286a787d759c9982385e510bca52f9
Magda Gutowska <m.gutowska0909@gmail.com>
2018-05-26 23:29:56 +0200

    Add files

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  	X-Client-Id: 3029
+	X-Auth-Token: 024403b39db97eeafc1c4c8e7b71b89a
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	})
+}
