27ead850c36ce38099ff390defbb9df585b555ab
Michał Gacek <gacek.michael@outlook.com>
2018-05-27 10:33:14 +0200

    Fixed.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  	X-Client-Id: 3029
-	X-Auth-Token: 024403b39db97eeafc1c4c8e7b71b89a
+  'X-Client-Id': 3029,
+	'X-Auth-Token': '024403b39db97eeafc1c4c8e7b71b89a',
 };
 
 $.ajaxSetup({
@@ -9,24 +9,24 @@ $.ajaxSetup({
 });
 
 $.ajax({
-    url: baseUrl + '/board',
-    method: 'GET',
-    success: function(response) {
-      setupColumns(response.columns);
-    }
+  url: baseUrl + '/board',
+  method: 'GET',
+  success: function(response) {
+    setupColumns(response.columns);
+  }
 });
 
 function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
-        board.createColumn(col);
-        setupCards(col, column.cards);
-    });
+  columns.forEach(function(column) {
+		var col = new Column(column.id, column.name);
+    board.createColumn(col);
+    setupCards(col, column.cards);
+  });
 }
 
 function setupCards(col, cards) {
 	cards.forEach(function (card) {
-        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    	col.createCard(cardObj);
-  	})
+    var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    col.createCard(cardObj);
+  });
 }
